62c2cabf3075fa47466c562f04fcdfbacbf055b3
unknown <shaha@WASJ45474.amtrak.ad.nrpc>
2015-08-11 09:46:53 -0400

    verify application jason header is present in response

--- src/test/java/HelloMavenTest.java
+++ src/test/java/HelloMavenTest.java
@@ -1,4 +1,7 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 
@@ -11,6 +14,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     runFlowAndExpect("sayHello", "HelloMaven");
     }
     
+    @Test
+    public void retrieveFlightsAddAppropraiteHeader() throws Exception {
+    	MuleEvent event = runFlow ("retrieveFlights");
+    	String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    	assertEquals("application/json", contentType);
+    			
+    }
 	
 	@Override
 	protected String getConfigFile() {
